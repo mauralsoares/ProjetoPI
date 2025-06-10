d6a929274238526bf11872d7338ca3a87214a857
mauralsoares <mlass@iscte-iul.pt>
2025-06-10 17:21:43 +0100

    feat: page resumos

---
+++ frontend/src/components/ResumoCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ResumoCard.css";
+
+const ResumoCard = ({resumo}) => {
+    return(
+        <div className="resumo-card">
+            <h3>{resumo.titulo}</h3>
+            <p className="resumo-autor">{resumo.ownerEmail}</p>
+            <div className="resumo-rating">
+                ⭐ {resumo.rating?.toFixed(1) || "0,0"} ({resumo.ratingCount || 0})
+            </div>
+            <p className="resumo-desc">{resumo.descricao}</p>
+            <a href={'http://localhost:4000/api/files/${resumo.id}/download'} className="resumo-btn" target="_blank" rel="noreferrer">
+                Download PDF
+            </a>
+        </div>
+
+    );
+};
+
+export default ResumoCard;
--- frontend/src/pages/Resumos.jsx
+++ frontend/src/pages/Resumos.jsx
@@ -1,13 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "../assets/css/Resumos.css";
-
+import axios from "axios";
 
 const Resumos = () => {
+    const[ucs, setUcs] = useState([]);
+    const[search, setSearch] = useState("");
+    const[filtered, setFiltered] = useState([]);
+    const[showDropdown, setShowDropdown] = useState(false);
+    const [resumos, setResumos] = useState([]);
+
+
+    useEffect(() => {
+        axios.get("http://localhost:4000/api/lists/ucs")
+            .then(res => {
+                setUcs(res.data);
+                console.log("UCs recebidas:", res.data);
+            })
+            .catch(err => {
+        console.error("Erro ao buscar UCs:", err);
+      });
+    }, []);
+
+    useEffect(() => {
+        if (search.trim() === "") {
+            setFiltered([]);
+            setShowDropdown(false);
+        } else {
+            const results = ucs.filter((uc) =>
+                String(uc).toLowerCase().includes(search.toLowerCase())
+            );
+            setFiltered(results);
+            setShowDropdown(true);
+        }
+    }, [search, ucs]);
+
+    const handleSelect = (uc) => {
+        setSearch(uc);
+        setShowDropdown(false);
+    }
+
+    useEffect(() => {
+        if (search.trim() === "") return;
+        axios.get(`http://localhost:4000/api/files/search?uc=${encodeURIComponent(search)}`)
+            .then(res => {
+            setResumos(res.data.files);
+            console.log("Resumos recebidos:", res.data.files);
+        })
+        .catch(err => {
+            console.error("Erro ao buscar resumos:", err);
+        });
+    }, [search]);
+
     return(
-        <div className="escolher-cadeira-container">
-        <h1>Escolha uma Cadeira</h1>
-        {/* Adiciona aqui a lógica e UI do seletor de cadeiras */}
+        <div className="resumos-container">
+            <h1>Fórum de Resumos</h1>
+            <input
+                type="text"
+                placeholder="Pesquisa uma cadeira..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onFocus={() => search && setShowDropdown(true)}
+                className="resumos-search"
+            />
+            {showDropdown && filtered.length > 0 &&(
+                <div className="resumos-dropdown">
+                    {filtered.map((uc, index) => (
+                        <div
+                        key={index}
+                        className="resumos-dropdown-items"
+                        onClick={() => handleSelect(uc)}
+                        >
+                            {uc}
+                        </div>
+                    ))}
+                </div>
+            )}
+            <div className="resumo-list">
+                {resumos.map((resumo) => (
+                    <ResumoCard key={resumo._id} resumo={resumo} />
+                ))}
+            </div>
         </div>
     );
 };
