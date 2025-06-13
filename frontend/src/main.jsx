a85edf3efc68a6ab2f1e022661186583dbede28c
Tomas-Manarte <tmcme@iscte-iul.pt>
2025-06-13 11:51:04 +0100

    hotfiz(perfil.jsx): forder do avatar melhorada padding das caixas melhorado

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './assets/scss/custom.scss';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
